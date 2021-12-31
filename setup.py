31f032f1b157dfdf41c6e2e85db74fe8c28dfd5f
Benny Chew <siansiew@gmail.com>
2021-12-31 22:40:06 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynamodump",
-    version="1.4.7",
+    version="1.5.0",
     author="Benny Chew",
     author_email="noreply@bennychew.com",
     description="Simple backup and restore for Amazon DynamoDB using AWS SDK for Python (boto3)",
