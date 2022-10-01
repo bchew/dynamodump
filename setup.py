e5f686a8f58d3423ca5f03734adfea4eecf15e3f
Benny Chew <siansiew@gmail.com>
2022-10-01 16:10:28 +1000

    chore: drop support for EOL python 3.6 (#159)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
     ],
     packages=["dynamodump"],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=["boto3==1.21.32", "six==1.16.0"],
     entry_points={
         "console_scripts": ["dynamodump=dynamodump.dynamodump:main"],
