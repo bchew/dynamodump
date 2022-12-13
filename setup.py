c53b47e3f723048147b36216fa8ef306b2a20148
Benny Chew <siansiew@gmail.com>
2022-12-13 16:13:50 +1100

    chore: bump version to 1.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynamodump",
-    version="1.6.1",
+    version="1.7.0",
     author="Benny Chew",
     author_email="noreply@bennychew.com",
     description="Simple backup and restore for Amazon DynamoDB using AWS SDK for Python (boto3)",
